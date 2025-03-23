b497456278f9f278926301eae55164551ba0fd58
kamil-1929 <kamil.329@outlook.com>
2025-03-23 15:15:26 +0000

    GenAI_Ollama

---
+++ LangChain Projects/Llama3.1_GenAI/ColdEmailProject/backend/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return "Cold Email Generator API is running"
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
+    
+
